77864ed8527468dfde2e17bfedd2be104e587907
jmuzy <jmuzyka4@gmail.com>
2025-10-18 15:26:12 -0400

    Pls help

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include "Client.h"
 
+#include "sockpp/tcp_connector.h"
+
 int main(int argc, char* argv[])
 {
     std::string IP = "127.0.0.1";
     Client player(IP);
+
+    int16_t port = 12345;
+    sockpp::initialize();
+    sockpp::tcp_connector conn;
+
+
     return 0;
 }
